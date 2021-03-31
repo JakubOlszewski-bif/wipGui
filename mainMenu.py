84ca17cd2cebad83558367bed10da2f8dbcb7c63
Jakub Olszewski <kubacki0088@gmail.com>
2021-03-31 15:48:03 +0200

    Change how indexes are created in addCommandToTree; Add "Move Up/Down" buttons for pipeline

--- mainMenu.py
+++ mainMenu.py
@@ -4,11 +4,13 @@ from os import path, makedirs
 TREE_COMMAND_INDEX = 0 #Index for pipeline-tree widget, used to mark each command's key so multiple same commands can be in the tree
 SAVE_PATH = "pipelines"
 
+# Create "pipelines" directory
 try:
     makedirs(SAVE_PATH)
 except FileExistsError:
     pass
 
+# Empty class for the container with last command
 class emptyClass():
     def __init__(self):
         self.key = ''
@@ -17,6 +19,7 @@ class emptyClass():
 
 pipelineTreeContent = emptyClass()
 
+# Create popup window with command
 def popupWig(master,widget,key):
     """Uses winBuild to create a pop-up window with instructions from widget and key.
 
@@ -31,11 +34,13 @@ def popupWig(master,widget,key):
     global pipelineTreeContent
     pipelineTreeContent = winBuild(newWindow,widget,key)
 
+# Main Menu 
 root = Tk()
 root.title("Main Menu")
 root.geometry("500x400")
 menuBar = Menu(root)
 
+# Menu Bar 
 commands = Menu(menuBar,tearoff=0)
 for package in commandWig.keys():
     subMenu = Menu(commands,tearoff=0)
@@ -46,41 +51,52 @@ menuBar.add_cascade(label = "Commands",menu = commands)
 
 root.config(menu=menuBar)
 
-### Pipeline - tree section ### 
+### Pipeline section ###
+
+# TreeView set-up
 tree = ttk.Treeview(root, columns = ("Stuff"))
 tree.heading("#0", text = "Command")
 tree.heading("#1", text = "Content")
 tree.pack() 
 
+# Add last command to tree
 def addCommandToTree(key,reqWidgets,optWidgets):
     global TREE_COMMAND_INDEX
-    commandKey = key + "$" + str(TREE_COMMAND_INDEX)
     TREE_COMMAND_INDEX+=1
-    tree.insert(parent = "", index = "end", iid = commandKey, text = key, values = "")
-    print("iid: ", commandKey, "\nkey: ", key, "\nreqWidgets: ",reqWidgets,"\noptWidgets: ",optWidgets)
+    parentID = tree.insert(parent = "", index = "end", text = key, values = "")
     for parameter,value in reqWidgets:
-        tree.insert(parent = commandKey, index = "end", text = parameter, values = value)
+        tree.insert(parent = parentID, index = "end", text = parameter, values = value)
     if optWidgets:
-        optIID = commandKey + "optional"
-        tree.insert(parent = commandKey, index = "end", iid = optIID, text = "Optional")
+        subParentID = tree.insert(parent = parentID, index = "end", text = "Optional")
         for parameter,value in optWidgets:
-            tree.insert(parent = optIID, index = "end", text = parameter, values = value)
+            tree.insert(parent = subParentID, index = "end", text = parameter, values = value)
 
+def handleCommand():
+    """Function for the button; uses addCommandToTree to add command to tree, then resets pipelineTreeContent to empty state 
+    """
+    global pipelineTreeContent
+    if pipelineTreeContent.key:
+        addCommandToTree(pipelineTreeContent.key,pipelineTreeContent.reqValues,pipelineTreeContent.optValues)
+        pipelineTreeContent = emptyClass()
+
+showMeContentForTree = Button(master = root, text = "Add last command to pipeline", command = handleCommand)
+showMeContentForTree.pack()
+
+# Edit selected content in pipeline
 def selectbranch(event):
     """Double-click event; when you doubleclick after selecting a cell with content, 
     it creates an Edit Window, where you can change the selected value.
     """
-    #Create editWindow only for columns with content
+    # Create editWindow only for columns with content
     clmn = tree.identify_column(event.x)
     if clmn != "#1":
-        return False
+        return 
     content = tree.selection()
     try:
-        #print("here")
         treeItem = tree.item(content)
         treeItemValue = treeItem["values"][0]
     except IndexError:
-        return False
+        return 
     #print("content: ",content, "\ntreeItem: ", treeItem)
     editWindow = Toplevel(root)
     editWindow.title("Edit parameter")
@@ -97,50 +113,82 @@ def selectbranch(event):
 
 tree.bind('<Double-1>', selectbranch)
 
-def handleCommand():
-    """Function for the button; uses addCommandToTree to add command to tree, then resets pipelineTreeContent to empty state 
-    """
-    global pipelineTreeContent
-    if pipelineTreeContent.key:
-        addCommandToTree(pipelineTreeContent.key,pipelineTreeContent.reqValues,pipelineTreeContent.optValues)
-        pipelineTreeContent = emptyClass()
-
-showMeContentForTree = Button(master = root, text = "Add last command to pipeline", command = handleCommand)
-showMeContentForTree.pack()
-
-### Get children ###
+# Run pipeline in bash
 def runTree():
     """Function for "Run pipeline" button in the pipeline widget. Pull all functions and parameters in the tree and passes them to bash.
-    Currently only prints commands instead of running in bash.
+    Currently only prints commands instead of running in bash. 
+    Allows starting pipeline from selected command instead of from the beginning.
     """
     mainBranches = tree.get_children()
+    sP = 0 # Starting Point
+    if sPC_Value.get() == 1:
+        try:
+            currentSel = tree.focus()
+            # Search for main branch
+            while currentSel not in mainBranches:
+                currentSel = tree.parent(currentSel)
+            sP = mainBranches.index(currentSel)
+        except ValueError:
+            pass
     commandsInPipeline = []
-    #print(mainBranches)
-    for commandIds in mainBranches:
-        subprocessPrintOut = 'qiime ' + commandIds.split("$")[0]
-        print(commandIds.split("$")[0])
-        command = tree.get_children(commandIds)
-        print("Required:")
-        for ids in command:
-            parameter = tree.item(ids)
-            if not parameter['values']:
-                optionalParameters = tree.get_children(commandIds+"optional")
-                print("Optional:")
-                for subIds in optionalParameters:
-                    parameter = tree.item(subIds)
-                    subprocessPrintOut = subprocessPrintOut + ' ' + str(parameter['text']) + ' ' + str(parameter['values'][0])
-                    print(parameter['text'],parameter['values'][0])
+    #rc = 0
+    for branch in mainBranches[sP:]:
+        command = 'qiime'
+        command = command + ' ' + tree.item(branch)["text"]
+        children = tree.get_children(branch)
+        for child in children:
+            currentChild = tree.item(child)
+            if currentChild["text"] == 'Optional':
+                optChildren = tree.get_children(child)
+                for optChild in optChildren:
+                    currOptChild = tree.item(optChild)
+                    command = command + ' ' + currOptChild["text"] + ' ' + str(currOptChild["values"][0])
             else:
-                subprocessPrintOut = subprocessPrintOut + ' ' + str(parameter['text']) + ' ' + str(parameter['values'][0])
-                print(parameter['text'],parameter['values'][0])
-        commandsInPipeline.append(subprocessPrintOut)
+                command = command + ' ' + currentChild["text"] + ' ' + str(currentChild["values"][0])
+        commandsInPipeline.append(command)
     for printoutstuff in commandsInPipeline:
         print(printoutstuff,end='\n')
+        '''
+        if rc != 0:
+            return
+        lista = printoutstuff.split()
+        child = subprocess.run(lista, stderr = subprocess.Pipe)
+        rc = child.returncode
+        
+        '''
+    print("\n\n")
 
 
-childButton = Button(master = root, text = "Run", command = runTree)
-childButton.pack()
-###
+# Move up
+def up():
+    current = tree.selection()
+    if current:
+        tree.move(current, tree.parent(current),tree.index(current)-1)
+# Move down
+def down():
+    current = tree.selection()
+    if current:
+        tree.move(current, tree.parent(current),tree.index(current)+1)
+
+
+## Container for pipeline window control buttons
+controlFrame = Frame(master = root)
+controlFrame.pack()
+
+sPC_Value = IntVar() # Interger value for startingPointCheck
+startingPointCheck = Checkbutton(master = controlFrame, text = "Start from selected", variable = sPC_Value, onvalue=1, offvalue = 0)
+startingPointCheck.grid(row = 1, column = 2)
+runButton = Button(master = controlFrame, text = "Run", command = runTree)
+runButton.grid(row = 1, column = 1)
+
+upButton = Button(master = controlFrame, text = "Move up", command = up)
+upButton.grid(row = 2, column = 1,sticky = E)
+
+downButton = Button(master = controlFrame, text = "Move down", command = down)
+downButton.grid(row = 2, column = 2, sticky = W)
+
+
+# Save pipeline to file
 def saveTree():
     #fileName = "test.txt"
     #completeName = path.join(SAVE_PATH,fileName)
@@ -177,16 +225,15 @@ def saveTree():
 saveButton = Button(master = root, text = "Save", command = saveTree)
 saveButton.pack()
 
-###
+# Open pipeline from file
 def openTree():
     completeName = filedialog.askopenfilenames(initialdir = './', title = "Select file", filetypes = (("all files","*.*"),("txt files","*.txt")))
     if completeName == '':
         return
-    # try:
-    #     fileTree = open(completeName[0], 'r')
-    # except FileNotFoundError:
-    #     return 
-    fileTree = open(completeName[0], 'r')
+    try:
+        fileTree = open(completeName[0], 'r')
+    except IndexError:
+        return
     tree.delete(*tree.get_children())
     key = ''
     reqWidgets = []
@@ -222,10 +269,8 @@ def openTree():
             action = action%6 + 1
     fileTree.close()
 
-
 openPipeButton = Button(master = root, text = "Open", command = openTree)
 openPipeButton.pack()
+### End of pipeline section ###
 
-
-###
 root.mainloop()
